ce7b55d099b81f930d80ece8bcddcaa7c7d041da
squareboyy <hikih4290@gmail.com>
2022-10-23 14:19:53 +0300

    Start learning git

---
+++ GitTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitTest
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
